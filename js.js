153f8c7cc2dbad70c1c28eeba5e8a7dd84510dc3
shb0110 <99003926+shb0110@users.noreply.github.com>
2022-04-18 11:11:04 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+
+alert("salom hush kelibsiz")
+const ism = prompt("ismingiz nima?")
+console.log(ism)
+document.write(`<h1>Salom <mark>${ism}</mark> hush kelibsiz</h1>`)
+alert(`salom  ${ism} hush kelibsiz`)
+
+const yosh = prompt("yoshingiz nechida?")
+console.log(yosh)
+document.write(`<h1>Salom ${ism} yoshingiz  <strong>${yosh}</strong> mi?</h1>`)
+confirm(`salom ${ism} yoshingiz  ${yosh} mi?`)
+
+const joy = prompt("qayerda yashaysiz?")
+console.log(joy)
+confirm(`salom ${ism} yoshingiz ${yosh} mi?.siz ${joy} da yashaysizmi?`)
+
+
+
